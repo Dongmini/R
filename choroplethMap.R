2292f14d25e4068208f85dc7f5c440c9685b9cc4
Dongmini <30786827+Dongmini@users.noreply.github.com>
2019-12-02 12:28:11 +0900

    Add files via upload

--- choroplethMap.R
+++ choroplethMap.R
@@ -1,28 +1,3 @@
-# R
-
-### 김동민
-
-# 저는 평범한 학생입니다.
-
-### 빅데이터학과
-
-# 빅데이터학과에서는 데이터를 다룹니다.
-
-### 좋아하는 것들
-+ 친구
-  - 승훈이형
-
-+ 애착 물건
-  - 컴퓨터
-
-+ 음식
-  - 국밥
-
-### 사진
-
-![](dasd.jpg)
-
-### 자료분석
 ### 지도 관련 패키지 설치 및 불러오기
 # + 공간 지도 분석을 위한 패키지
 # - maps: 세계 지도 데이터베이스
@@ -74,15 +49,29 @@ library(stringr)
 kormap2
 str(kormap2)
 
-### 자료 분석2
-강원도 도시대기측정결과를 지도에 표현해보자.
+kangwon_air_201904 <- read.csv("kangwon_air_201904.csv")
+kangwon_air_201904 <- rename(kangwon_air_201904,
+                             city = 시군,
+                             station = 측정소)
 
-### 지도 관련 패키지 설치 및 불러오기
-# + 공간 지도 분석을 위한 패키지
-# - maps: 세계 지도 데이터베이스
-# - mapproj: 지도 상에 위도와 경도를 표시
-# - ggplot2: map_data()를 이용하여 지도정보를 R로 불러오기
-# - ggiraphExtra : 단계 구분도 표시
+kangwon_map <- read.csv("kangwon_map.csv")
+kangwon_map <- kormap2 %>% filter(str_starts(region, "32"))
+map('korea_map', region=c('32'))
 
-ggplot(economics, aes(x=date, y=pce)) + geom_line(color='red')
+
+ggChoropleth(data = kangwon_air_201904,     # 지도에 표현할 데이터
+             mapping=aes(fill = PM2.5,        # 색깔로 표현할 변수
+                         map_id = region, # 지역 기준 변수, region
+                         tooltip = name),  # 지도 위에 표시할 지역명
+             title = "강원도 2019년 4월 미세먼지 PM2.5 분포",
+             map = kangwon_map,             # 지도 데이터
+             interactive = T)    
+
+
+ggplot(kangwon_air_201904,aes(map_id=region,fill=PM2.5))+
+  geom_map(map=kangwon_map,colour="black",size=0.1)+
+  expand_limits(x=kangwon_map$long,y=kormap1$lat)+
+  scale_fill_gradientn(colours=c('white','orange','red'))+
+  ggtitle("2015년도 시도별 인구분포도")+
+  coord_map()
 
